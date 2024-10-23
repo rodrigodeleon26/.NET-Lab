6bcfd73b0a357895197d00c47a1341be5d76f0ce
pasantiasutecimsj <pasantiasutecimsj@gmail.com>
2024-10-23 11:51:10 -0300

    Update DAL_CitasMedicas_EF.cs

--- DAL/DALs/DAL_CitasMedicas_EF.cs
+++ DAL/DALs/DAL_CitasMedicas_EF.cs
@@ -134,7 +134,7 @@ namespace DAL.DALs
                 var citaEntity = new CitasMedicas
                 {
                     Fecha = nuevaCita.Fecha,
-                    Estado = nuevaCita.Estado ?? "Agendada",
+                    Estado = nuevaCita.Estado ?? "AGENDADA",
                     PacienteId = pacienteId,
                     CalendarioId = calendarioId
                 };
