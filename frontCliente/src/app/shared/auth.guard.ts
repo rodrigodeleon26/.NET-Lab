6f0931e97f8f27046d138da078626ca6257279e9
Rodrigo de León <rodrigo.deleon@estudiantes.utec.edu.uy>
2024-10-27 18:08:01 -0300

    Update AuthGuard.ts en frontCliente

--- frontCliente/src/app/shared/auth.guard.ts
+++ frontCliente/src/app/shared/auth.guard.ts
@@ -6,12 +6,14 @@ export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
+  // Si no está autenticado, redirige a /login
   if (!authService.isLoggedIn()) {
     router.navigate(['/login']);
     return false;
   }
 
-  if (!authService.getEmailConfirmedStatus()) {
+  // Si el correo no está confirmado y la ruta actual no es /resendEmailConfirmation
+  if (!authService.getEmailConfirmedStatus() && state.url !== '/resendEmailConfirmation') {
     router.navigate(['/resendEmailConfirmation']);
     return false;
   }
